443f16d54434a0ad5b386c529517db3cf5634a96
Ray Chang <rayselfs@gmail.com>
2026-08-28 12:54:49 +0800

    fix: allow content import job in release what-if (#45)

--- scripts/check-what-if.sh
+++ scripts/check-what-if.sh
@@ -13,6 +13,8 @@ jq -e '
     | select((
         (.changeType == "Modify" and (.resourceId | endswith("/Microsoft.App/containerApps/hhc-web-api") or endswith("/Microsoft.App/jobs/hhc-web-migrate")))
         or
+        ((.changeType == "Create" or .changeType == "Modify") and (.resourceId | endswith("/Microsoft.App/jobs/hhc-web-content-import")))
+        or
         ((.changeType == "Create" or .changeType == "Modify") and (.resourceId | endswith("/Microsoft.CognitiveServices/accounts/bible-text-embedding-resource/raiPolicies/hhc-cms-translation-v1")))
       ) | not)
   ] | length == 0)
